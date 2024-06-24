045baff88344969dbd139b5b080c730567d0694a
dewa-mahendra-laptop <mahendraputra21@gmail.com>
2024-06-24 18:57:04 +0800

    51. Develop POST Endpoint with Carter implements ICarterModule for Minimal Apis

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,10 +1,16 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using Carter;
+
+namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
 
     public record CreateProductResponse(Guid Id);
 
-    public class CreateProductEndpoint
+    public class CreateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
