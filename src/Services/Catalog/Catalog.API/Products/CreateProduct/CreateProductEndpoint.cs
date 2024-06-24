a54ec64a0f7a5d5eb22c92903f3612870b5117e2
dewa-mahendra-laptop <mahendraputra21@gmail.com>
2024-06-24 18:51:59 +0800

    50. Develop Create Product Endpoint with Minimal Apis and Carter

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,9 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+    public record CreateProductResponse(Guid Id);
+
     public class CreateProductEndpoint
     {
     }
