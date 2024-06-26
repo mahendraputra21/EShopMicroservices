f7165258a060888c8e9fe7f426833131b7556d86
Dewa Mahendra <d.putra@lakeba.com>
2024-06-26 15:45:49 +0800

    84. Cross-cutting Concerns: Validate with AbstractValidator using Fluent Validation

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,15 +1,37 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿using FluentValidation;
+
+namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler(IDocumentSession session)
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Image File is required");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price is required");
+    }
+}
+
+internal class CreateProductCommandHandler
+    (IDocumentSession session, IValidator<CreateProductCommand> validator)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        // validate the request first by validator
+        var result = await validator.ValidateAsync(command, cancellationToken);
+        var errors = result.Errors.Select(e => e.ErrorMessage).ToList();
+        if (errors.Any()) 
+        {
+            throw new ValidationException(errors.FirstOrDefault());
+        }
+
         // create Product entity from command object
         var product = new Product
         {
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -6,10 +6,15 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
+
+
 
 var app = builder.Build();
 
