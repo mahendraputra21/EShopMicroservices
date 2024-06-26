9ab6a11213e94e8ebd5613d75905a1fd0ec4dc7f
Dewa Mahendra <d.putra@lakeba.com>
2024-06-26 12:05:23 +0800

    77. Test UpdateProduct Endpoint Connecting to Docker Postgres Container

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -1,6 +1,27 @@
 ﻿namespace Catalog.API.Products.UpdateProduct
 {
-    public class UpdateProductEndpoint
+    public record UpdateProductRequest(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record UpdateProductResponse(bool IsSuccess);
+
+    public class UpdateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateProductCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<UpdateProductResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("UpdateProduct")
+            .Produces<UpdateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Update Product")
+            .WithDescription("Update Product");
+        }
     }
 }
