dc40ef0736743866e983e389aa1e33101572006c
Dewa Mahendra <d.putra@lakeba.com>
2024-06-26 17:21:31 +0800

    89. BuildingBlocks Handling Exceptions Generic Way w/ app.UseExceptionHandler

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -15,7 +15,7 @@ internal class GetProductByIdQueryHandler
 
         if(product is null)
         {
-            throw new ProductNotFoundException();
+            throw new ProductNotFoundException(query.Id);
         }
 
         return new GetProductByIdResult(product);
--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -34,7 +34,7 @@ namespace Catalog.API.Products.UpdateProduct
 
             if(product is null)
             {
-                throw new ProductNotFoundException();
+                throw new ProductNotFoundException(command.Id);
             }
 
             product = command.Adapt<Product>();
