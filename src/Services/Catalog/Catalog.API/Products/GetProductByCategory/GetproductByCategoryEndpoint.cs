4b6edee69f053f01ae086de75c03728c2e49912d
Dewa Mahendra <d.putra@lakeba.com>
2024-06-26 11:30:29 +0800

    74. Test GetProductByCategory Endpoint Connecting to Docker Postgres Container

--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetproductByCategoryEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetproductByCategoryEndpoint.cs
@@ -1,5 +1,28 @@
-﻿namespace Catalog.API.Products.GetProductByCategory;
+﻿
+using Catalog.API.Products.CreateProduct;
 
-public class GetproductByCategoryEndpoint
+namespace Catalog.API.Products.GetProductByCategory;
+
+//public record GetProductByCategoryRequest();
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetproductByCategoryEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", 
+            async (string category, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByCategoryQuery(category));
+            
+            var response = result.Adapt<GetProductByCategoryResponse>();
+            
+            return Results.Ok(response);
+        })
+        .WithName("GetProductByCategory")
+        .Produces<GetProductByCategoryResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Category")
+        .WithDescription("Get Product By Category");
+    }
 }
