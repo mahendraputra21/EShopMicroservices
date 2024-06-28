2726f043c3630086f28143b3364809b4aa97de33
Dewa Mahendra <d.putra@lakeba.com>
2024-06-28 12:06:27 +0800

    113. Develop DeleteBasket Feature Handler class with CQRS and MediatR

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,5 +1,24 @@
-﻿namespace Basket.API.Basket.DeleteBasket;
+﻿
+namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketHandler
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
 {
+    public DeleleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+internal class DeleteBasketCommandHandler
+    : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        // TODO: delete basket from database and cache
+
+        return new DeleteBasketResult(true);
+    }
 }
