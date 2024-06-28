05ece897cd620caf5df8b20b2ad9d6fa64a8808b
Dewa Mahendra <d.putra@lakeba.com>
2024-06-28 11:59:42 +0800

    112. Develop Store Basket Endpoint with Minimal Apis and Carter

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -1,5 +1,29 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+﻿
+using Basket.API.Basket.GetBasket;
 
-public class StoreBasketEndpoint
+namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketRequest(ShoppingCart Cart);
+public record StoreBasketResponse(string UserName);
+
+public class StoreBasketEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<StoreBasketCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<StoreBasketResponse>();
+
+            return Results.Created($"/basket/{response.UserName}", response);
+        })
+        .WithName("StoreBasket")
+        .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Store Basket")
+        .WithDescription("Store Basket");
+    }
 }
