aa770b9c435bf388e5c1c0c7e108ef14589c9da7
Dewa Mahendra <d.putra@lakeba.com>
2024-06-28 11:22:04 +0800

    109. Develop GetBasket Feature Handler class with CQRS and MediatR

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,5 +1,16 @@
-﻿namespace Basket.API.Basket.GetBasket;
+﻿
+namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketHandler
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingCart Cart);
+
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        //TODO: get basket from database
+
+        return new GetBasketResult(new ShoppingCart("swn"));
+    }
 }
