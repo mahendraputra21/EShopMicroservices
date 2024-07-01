13da3f5de15cb35a0862bb9db5b96e26bed58e28
Dewa Mahendra <d.putra@lakeba.com>
2024-07-01 13:27:37 +0800

    130. Develop CachedBasketRepository with Proxy Pattern, Decorator Pattern and Scrutor

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Basket.API.Data;
+
+public class CachedBasketRepository (IBasketRepository basketRepository)
+    : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+       return await basketRepository.GetBasket(userName, cancellationToken);
+    }
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        return await basketRepository.StoreBasket(basket, cancellationToken);
+    }
+
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        return await basketRepository.DeleteBasket(userName, cancellationToken);
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 
