b69774bfdd463cb4d13ee4da0aad51077f12ae9d
Dewa Mahendra <d.putra@lakeba.com>
2024-07-01 11:23:23 +0800

    119. Implement IBasketRepository Class using Marten IDocumentSession interface

--- src/Services/Basket/Basket.API/Data/BasketRepository.cs
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -1,20 +1,25 @@
-﻿
-namespace Basket.API.Data;
+﻿namespace Basket.API.Data;
 
-public class BasketRepository : IBasketRepository
+public class BasketRepository(IDocumentSession session) 
+    : IBasketRepository
 {
-    public Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var basket = await session.LoadAsync<ShoppingCart>(userName, cancellationToken);
+        return basket is null ? throw new BasketNotFoundException(userName) : basket;
     }
 
-    public Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+       session.Store(basket);
+       await session.SaveChangesAsync(cancellationToken);
+       return basket;
     }
 
-    public Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        session.Delete<ShoppingCart>(userName);
+        await session.SaveChangesAsync(cancellationToken);
+        return true
     }
 }
--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -5,3 +5,6 @@ global using MediatR;
 global using Mapster;
 global using FluentValidation;
 global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions;
+global using Basket.API.Exceptions;
+global using Marten;
